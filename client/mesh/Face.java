6d5912088d405d28c8100044ab24b4c630aca683
gujadot <gujadot@gmail.com>
2015-03-10 11:16:20 +0100

    added file for test 3d renderer

---
+++ client/mesh/Face.java
@@ -0,0 +1,5 @@
+package anonmine.beastmod.client.mesh;
+
+public class Face {
+
+}
---
+++ client/mesh/Mesh.java
@@ -0,0 +1,13 @@
+package anonmine.beastmod.client.mesh;
+
+import java.util.ArrayList;
+
+import org.lwjgl.util.vector.Vector3f;
+
+import scala.collection.immutable.List;
+
+public class Mesh {
+
+	public ArrayList<Vector3f> vertices =  new ArrayList<Vector3f>();
+	public ArrayList<Vector3f> normals =  new ArrayList<Vector3f>();
+}
